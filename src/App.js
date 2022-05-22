0247991aac91b1d2e62285a583f59c462dc4de7b
Golam Mursalin <golammursalin309@gmail.com>
2022-05-23 01:17:38 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+// React
+import { Fragment } from "react";
 // Components
+import Home from "./components/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+import Products from "./components/Products/Products";
 // React Routes
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-5xl">Hi There</h1>
-      <button class="btn btn-active btn-primary">Button</button>
-    </div>
+    <Fragment>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </Fragment>
   );
 }
 
