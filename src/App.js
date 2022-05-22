bc1ac937cdf69b8a05521102e5907b0df645abe6
Golam Mursalin <golammursalin309@gmail.com>
2022-05-23 00:16:18 +0600

    project initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// Components
+// React Routes
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-5xl">Hi There</h1>
+      <button class="btn btn-active btn-primary">Button</button>
     </div>
   );
 }
