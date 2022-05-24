0ecc98c5bdf9c698705089aee256ea2d124a562c
Golam Mursalin <golammursalin309@gmail.com>
2022-05-24 15:54:02 +0600

    review section added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,11 +3,13 @@ import React from "react";
 // Components
 import Banner from "./Banner";
 import Tools from "./Tools";
+import Reviews from "./Reviews";
 const Home = () => {
   return (
     <>
       <Banner />
       <Tools />
+      <Reviews />
     </>
   );
 };
